af45492362f6d82a3172068efe4d4bc1c85162af
mogoodrich <mgoodrich@pih.org>
2012-09-19 10:48:40 -0400

    deleted Mirth configuration utils & tests because they have been moved into the MIrebalais module

--- api/src/main/java/org/openmrs/module/pacsintegration/PacsIntegrationGlobalProperties.java
+++ api/src/main/java/org/openmrs/module/pacsintegration/PacsIntegrationGlobalProperties.java
@@ -17,12 +17,6 @@ import org.openmrs.api.context.Context;
 
 public class PacsIntegrationGlobalProperties {
 
-    public static final String GLOBAL_PROPERTY_MIRTH_IP_ADDRESS() {
-        return Context.getAdministrationService().getGlobalProperty("pacsintegration.mirthIpAddress");
-    }
 
-    public static final Integer GLOBAL_PROPERTY_MIRTH_INPUT_PORT() {
-        return Integer.parseInt(Context.getAdministrationService().getGlobalProperty("pacsintegration.mirthInputPort"));
-    }
 
 }
--- api/src/main/resources/org/openmrs/module/pacsintegration/mirth/deployChannels.sh
+++
@@ -1,6 +0,0 @@
-
-java -classpath '/opt/mirthconnect/*:/opt/mirthconnect/cli-lib/*' com.mirth.connect.cli.launcher.CommandLineLauncher -a https://127.0.0.1:8443 -u mirth -p Mirth123 -v 0.0.0 < deployChannels.txt
-
-
-
-
