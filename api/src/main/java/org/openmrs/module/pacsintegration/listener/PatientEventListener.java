d85b0d8496b13fc5c3d3a95bd25724bf7e7cb905
Alexandre Barbosa <abarbos@thoughtworks.com>
2012-10-01 22:18:19 -0300

    #94: Integration tests for sending a created patient to PACS

--- api/src/main/java/org/openmrs/module/pacsintegration/listener/PatientEventListener.java
+++ api/src/main/java/org/openmrs/module/pacsintegration/listener/PatientEventListener.java
@@ -3,6 +3,7 @@ package org.openmrs.module.pacsintegration.listener;
 import org.openmrs.OpenmrsObject;
 import org.openmrs.Patient;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
 import org.openmrs.event.Event;
 import org.openmrs.event.SubscribableEventListener;
 import org.openmrs.module.pacsintegration.api.PacsIntegrationService;
@@ -11,9 +12,13 @@ import org.openmrs.module.pacsintegration.api.PatientToPacsConverter;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
 import javax.jms.Message;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.openmrs.event.Event.Action.CREATED;
+import static org.openmrs.event.Event.Action.UPDATED;
+import static org.openmrs.module.pacsintegration.PacsIntegrationGlobalProperties.LISTENER_PASSWORD;
+import static org.openmrs.module.pacsintegration.PacsIntegrationGlobalProperties.LISTENER_USERNAME;
 
 
 public class PatientEventListener implements SubscribableEventListener {
@@ -32,35 +37,40 @@ public class PatientEventListener implements SubscribableEventListener {
 
     @Override
     public List<Class<? extends OpenmrsObject>> subscribeToObjects() {
-        return null;
+        Object classes = Arrays.asList(Patient.class);
+        return (List<Class<? extends OpenmrsObject>>) classes;
     }
 
     @Override
     public List<String> subscribeToActions() {
-        return null;
+        return Arrays.asList(Event.Action.CREATED.name(), Event.Action.UPDATED.name());
     }
 
     @Override
     public void onMessage(Message message) {
+        Context.openSession();
         try {
+            Context.authenticate(LISTENER_USERNAME(), LISTENER_PASSWORD());
             MapMessage mapMessage = (MapMessage) message;
-            String action = mapMessage.getStringProperty("action");
-            String classname = mapMessage.getStringProperty("classname");
+            String action = mapMessage.getString("action");
+            String classname = mapMessage.getString("classname");
 
             boolean isPatient = Patient.class.getName().equals(classname);
             boolean isCreated = CREATED.toString().equals(action);
-            boolean isUpdated = Event.Action.UPDATED.toString().equals(action);
+            boolean isUpdated = UPDATED.toString().equals(action);
 
             if((isCreated || isUpdated) && isPatient)
                 sendToPacs(mapMessage);
 
         } catch (JMSException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
+        } finally {
+            Context.closeSession();
         }
     }
 
     private void sendToPacs(MapMessage mapMessage) throws JMSException {
-        String uuid = mapMessage.getStringProperty("uuid");
+        String uuid = mapMessage.getString("uuid");
         String sendingFacility = "";  //TODO We need to determine how the sending facility will be retrieved
 
         Patient patient = patientService.getPatientByUuid(uuid);
--- api/src/test/java/org/openmrs/module/pacsintegration/api/PatientToPacsIT.java
+++ api/src/test/java/org/openmrs/module/pacsintegration/api/PatientToPacsIT.java
@@ -1,11 +1,79 @@
 package org.openmrs.module.pacsintegration.api;
 
-/**
- * Created with IntelliJ IDEA.
- * User: abarbos
- * Date: 10/1/12
- * Time: 2:40 PM
- * To change this template use File | Settings | File Templates.
- */
-public class PatientToPacsIT {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openmrs.*;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.event.advice.GeneralEventAdvice;
+import org.openmrs.module.pacsintegration.listener.PatientEventListener;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.NotTransactional;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PatientToPacsIT extends BaseModuleContextSensitiveTest {
+
+    protected static final String XML_DATASET = "org/openmrs/module/pacsintegration/include/pacsIntegrationTestDataset.xml";
+
+    @Autowired
+    private PatientService patientService;
+    @Autowired
+    @InjectMocks
+    private PatientEventListener patientEventListener;
+    @Mock
+    private PacsIntegrationService pacsIntegrationService;
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        executeDataSet(XML_DATASET);
+    }
+
+
+    @Test
+    @NotTransactional
+    public void sendsCreatedPatientsToPacs() throws Exception {
+        Context.addAdvice(PatientService.class, new GeneralEventAdvice());
+
+        Patient patient = createPatient();
+        patientService.savePatient(patient);
+
+        verify(pacsIntegrationService, timeout(5000)).sendMessageToPacs(any(String.class));
+    }
+
+
+    private Patient createPatient() throws ParseException {
+        PatientIdentifierType identifierType = new PatientIdentifierType(1);
+        Location location = new Location(1);
+
+        PatientIdentifier patientIdentifier = new PatientIdentifier("PATIENT_IDENTIFIER", identifierType, location);
+        patientIdentifier.setIdentifier("PATIENT_IDENTIFIER");
+
+        PersonName patientName = new PersonName();
+        patientName.setFamilyName("Doe");
+        patientName.setGivenName("John");
+
+        Patient patient = new Patient();
+        patient.addIdentifier(patientIdentifier);
+        patient.addName(patientName);
+        Date birthDate = new SimpleDateFormat("MM-dd-yyyy").parse("08-27-1979");
+        patient.setBirthdate(birthDate);
+        patient.setGender("M");
+
+        return patient;
+    }
 }
